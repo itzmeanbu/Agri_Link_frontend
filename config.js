e06a6b1c942c196b490b5ad86770fe933fd165f7
ANBUSELVAN I <anbuilayaperumal@gmail.com>
2026-09-18 01:54:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.AGRILINK={name:'AgriLink',logo:'logo.jpg',apiBase:window.AGRILINK_API_URL||'http://localhost:5000',orderStatuses:['Pending','Confirmed','Shipped','Out for Delivery','Delivered','Cancelled'],paymentStatuses:['Pending','Paid','Failed','Refunded']};
+window.AGRILINK={name:'AgriLink',logo:'logo.jpg',apiBase:'https://your-backend-name.onrender.com',orderStatuses:['Pending','Confirmed','Shipped','Out for Delivery','Delivered','Cancelled'],paymentStatuses:['Pending','Paid','Failed','Refunded']};
